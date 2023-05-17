ef2fc2b772bf401599e105376156ed4b27523517
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-05-17 11:49:07 +0200

    chore(compatibility): set the supported ios to 13.0. set the swift version to 5.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PaylikeLuhn",
-    platforms: [.macOS(.v10_15), .iOS(.v9)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(name: "PaylikeLuhn", targets: ["PaylikeLuhn"]),
     ],
